9315ebe653e78d34656e6b224471f2eda1cb12e1
acelion55 <harshvardhan53394@gmail.com>
2026-09-27 01:00:54 +0530

    new set

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ require('dotenv').config({ path: path.join(__dirname, '.env') });
 const DEFAULT_MONGO_URI = 'mongodb://danishali840wj_db_user:jp7yRc7fWyPgIJQ3@ac-26udp0r-shard-00-00.lcwftcr.mongodb.net:27017,ac-26udp0r-shard-00-01.lcwftcr.mongodb.net:27017,ac-26udp0r-shard-00-02.lcwftcr.mongodb.net:27017/oxpay?ssl=true&replicaSet=atlas-z50mpv-shard-0&authSource=admin&appName=Cluster0';
 const MONGO_URI = process.env.MONGO_URI || DEFAULT_MONGO_URI;
 
+const authRoutes = require(path.join(__dirname, 'routes/authRoutes'));
+const adminRoutes = require(path.join(__dirname, 'routes/adminRoutes'));
+
 // Connect to MongoDB Atlas immediately
 mongoose
   .connect(MONGO_URI, {
@@ -25,13 +28,13 @@ mongoose
 
 mongoose.connection.on('connected', () => {
   console.log('Mongoose event: connected to Atlas MongoDB');
+  if (authRoutes && authRoutes.seedAdminToDb) {
+    authRoutes.seedAdminToDb();
+  }
 });
 mongoose.connection.on('error', (err) => {
   console.error('Mongoose event error:', err.message);
 });
-
-const authRoutes = require(path.join(__dirname, 'routes/authRoutes'));
-const adminRoutes = require(path.join(__dirname, 'routes/adminRoutes'));
 
 const app = express();
 
